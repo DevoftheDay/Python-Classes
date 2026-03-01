cf7215becab5df970932650e8d36e4eeb9c9d0e0
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-02-28 19:47:41 -0800

    data types lesson in python

---
+++ Data Types (3)/userinput.py
@@ -0,0 +1,3 @@
+number = input("Enter any number: ")
+print(number)
+print(type(number))
---
+++ Getting Started (2)/hw.py
@@ -0,0 +1,19 @@
+birthday = input("What is your birthday: ")
+
+print("\nYour birthday is on", birthday)
+
+mombirthday = input("What is your mom's birthday: ")
+
+print("\nYour mom's birthday is on", mombirthday)
+
+dadbirthday = input("What is your dad's birthday: ")
+
+print("\nYour dad's birthday is on", dadbirthday)
+
+siblingbirthday = input("What is your sibling's birthday: ")
+
+print("\nYour sibling's birthday is on", siblingbirthday)
+
+friendbirthday = input("What is your friend's birthday: ")
+
+print("\nYour friend's birthday is on", friendbirthday)
