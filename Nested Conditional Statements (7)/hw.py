54c8cca25eeb406c2f84655b20b433e63d9c1053
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-04-04 20:04:20 -0700

    nested conditional statements homework

---
+++ Nested Conditional Statements (7)/hw.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age here: "))
+
+if age>=10:
+    if age<=20:
+        print("Your age is ", age, " and you are eligible")
+    else:
+        print("You are not eligible")
+else:
+        print("You are not eligible")
+
+    
