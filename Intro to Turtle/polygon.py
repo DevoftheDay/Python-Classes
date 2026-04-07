d7756a95c14de92daebcd883ed5829908346965e
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-04-07 08:44:44 -0700

    intro to turtle

---
+++ Intro to Turtle/polygon.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+
+num_sides = 8
+side_length = 20
+angle = 360.0 / num_sides
+
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.left(angle)
+
+
+turtle.done()
