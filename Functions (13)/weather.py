2a9302aed13615f270b6e1a7622cf83542a1b074
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-04-07 09:39:25 -0700

    functions in py

---
+++ Functions (13)/weather.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print("The weather is pleasent in", spring)
+    print("THe weather is the same in", autumn)
+
+spring = "autumn"
+autumn = spring
+
+weather_condition()
