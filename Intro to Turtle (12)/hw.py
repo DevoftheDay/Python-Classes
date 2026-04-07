80ab6375f683ba4daab1e30011ba6e94b64ba8e6
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-04-07 11:20:46 -0700

    intro to turtle hw

---
+++ Intro to Turtle (12)/hw.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(400,400)
+square = turtle.Turtle()
+
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+
+turtle.done()
