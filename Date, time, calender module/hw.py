540ea7d5374742e9ce8104dc4aa2b32464a52cdc
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-04-22 20:12:26 -0700

    calendar module hw

---
+++ Date, time, calender module/hw.py
@@ -0,0 +1,28 @@
+import datetime
+
+time = datetime.datetime.now()
+
+if time.month == 1:
+    print("January")
+elif time.month == 2:
+    print("Febuary")
+elif time.month == 3:
+    print("March")
+elif time.month == 4:
+    print("April")
+elif time.month == 5:
+    print("May")
+elif time.month == 6:
+    print("June")
+elif time.month == 7:
+    print("July")
+elif time.month == 8:
+    print("August")
+elif time.month == 9:
+    print("September")
+elif time.month == 10:
+    print("October")
+elif time.month == 11:
+    print("November")
+elif time.month == 12:
+    print("December")
