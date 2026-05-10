c458a202f781d73bb924f44f4fa5f3b941715be9
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-05-09 20:40:11 -0700

    tuples

---
+++ Tuples (18)/operations.py
@@ -0,0 +1,20 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = (4,2,3,5,7,1,2)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (30,10,12,30,30,50)
+print(tuple1.count(30))
+
+tuplex = (2,4,3,5,4,6,7,8,6,1)
+
+slice = tuplex[3:5]
+print(slice)
+
+
+slice = tuplex[:6]
+print(slice)
