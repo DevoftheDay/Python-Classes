4c3ff7de928c428506125ed76a603b713e4c9244
SiddyG96 <siddharthshankaranganesh@gmail.com>
2026-06-16 09:11:36 -0700

    basic sets and arrays

---
+++ Sets and Arrays (20)/setintersection.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"red", "green"}
+print("original set elements: ")
+print(setx)
+print(sety)
+print("\nIntersection of two sets: ")
+setz = setx.intersection(sety)
+print(setz)
